f4382bc30fb5ce8eda3088dd98e97d542785d567
Valeria Durruty <vale_gelp87@hotmail.com>
2025-04-29 20:18:05 -0300

    Se sube archivo json de datos.

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'trailerflix_grupo4')));
-
-
 // Manejo de rutas inexistentes
 app.use((req, res) => {
     res.json({ error: 404, message: 'No se encuentra la ruta o el recurso solicitado.'});
